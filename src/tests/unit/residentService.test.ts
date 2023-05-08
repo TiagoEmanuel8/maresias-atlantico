1bfe23f6e1b3564632aa5c6a33207202378dca8d
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-05-08 08:58:42 -0300

    feat: definindo testes para camada services

---
+++ src/tests/unit/residentService.test.ts
@@ -0,0 +1,29 @@
+import * as sinon from 'sinon';
+import { expect } from 'chai';
+
+describe('Resident Service', () => {
+  describe('When creating a new resident', () => {
+    it('Creates a new resident is succesfully', () => {});
+    it('Fails to return an updated Resident - Zod Error');
+  })
+  describe('When searching all residents', () => {
+    it('Return all residents succesfully', () => {});
+    it('Fails to return an updated Resident - Zod Error');
+  });
+
+  describe('When searching a resident', () => {
+    it('Return resident succesfully', () => {});
+    it('Fails to return an updated Resident - Zod Error');
+  });
+
+  describe('When updating a resident', () => {
+    it('Updating resident is succesfully', () => {});
+    it('Fails to return an updated Resident - EntityNotFound');
+    it('Fails to return an updated Resident - Zod Error');
+  });
+
+  describe('When deleting a resident', () => {
+    it('Delete a resident succesfully', () => {});
+    it('Fails to return an updated Resident - Zod Error');
+  })
+})
