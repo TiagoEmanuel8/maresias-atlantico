f1dad7059de2c608c5e0335c3bb39b8f52fc00f0
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-05-08 09:02:58 -0300

    feat: definindo testes para camada models

---
+++ src/tests/unit/residentModel.test.ts
@@ -0,0 +1,25 @@
+import * as sinon from 'sinon';
+import { expect } from 'chai';
+
+describe('Resident Model', () => {
+  describe('When creating a new resident', () => {
+    it('Creates a new resident is succesfully', () => {});
+  })
+  describe('When searching all residents', () => {
+    it('Return all residents succesfully', () => {});
+  });
+
+  describe('When searching a resident', () => {
+    it('Return resident succesfully', () => {});
+    it('Fails to return a resident when id is not found');
+  });
+
+  describe('When updating a resident', () => {
+    it('Updating resident is succesfully', () => {});
+    it('Fails to return a resident when id is not found');
+  });
+
+  describe('When deleting a resident', () => {
+    it('Delete a resident succesfully', () => {});
+  })
+})
