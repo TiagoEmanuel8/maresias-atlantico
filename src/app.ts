54a9e361ba4d756157700bba2c1db7ef6863ea77
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-05-08 08:51:53 -0300

    feat: definindo testes para camada controller

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,8 @@
-import cors from 'cors';
+import 'express-async-errors';
 import express from 'express';
-import debug from 'debug';
 
-require('express-async-errors');
+const app = express();
 
-class App {
-  public app: express.Application = express();
-  public debugLog: debug.IDebugger = debug('app');
+app.use(express.json());
 
-  constructor() {
-    this.config();
-  }
-
-  private config():void {
-    const accessControl: express.RequestHandler = (_req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
-      res.header('Access-Control-Allow-Headers', '*');
-      next();
-    };
-
-    this.app.use(express.json());
-    this.app.use(accessControl);
-    this.app.use(cors());
-  }
-
-  public start(PORT: string | number):void {
-    this.app.listen(PORT, () => {
-      console.log('iniciado porta:', PORT);
-    });
-  }
-}
-
-export { App };
-
-export const { app } = new App();
+export default app;
---
+++ src/tests/unit/residentController.test.ts
@@ -0,0 +1,27 @@
+import * as sinon from 'sinon';
+import { expect } from 'chai';
+import { Request, Response } from 'express';
+
+
+describe('Tests Residents Controllers', () => {
+  describe('When creating a new resident', () => {
+    it('Creates a new resident succesfully', () => {});
+    it('It is not possible to register a resident without the block and apartment number', () => {});
+  });
+
+  describe('When searching all residents', () => {
+    it('Return all residents succesfully', () => {});
+  });
+
+  describe('When searching a resident', () => {
+    it('Return resident succesfully', () => {});
+  });
+
+  describe('When updating a resident', () => {
+    it('Updating resident is succesfully', () => {});
+  });
+
+  describe('When deleting a resident', () => {
+    it('Delete a resident succesfully', () => {})
+  })
+})
