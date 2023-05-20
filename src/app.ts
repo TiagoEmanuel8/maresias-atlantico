7413f5a3b2c2fc9d22e711b9ca88d6dc91ff0a9e
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-05-19 22:54:32 -0300

    fix: adicionando lib cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import 'express-async-errors';
+import cors from 'cors';
 import express from 'express';
 import { residentsRouter, swaggerRouter } from './routes/index.routes';
 import { errorMiddleware } from './middlewares/errorMiddleware';
 
 const app = express();
 require('express-async-errors');
-
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PACTH', 'DELETE'],
+}));
 app.use(express.json());
 app.use(swaggerRouter);
 app.use(residentsRouter);
